6b9948b4ea0c4e7d7b35a9351c660018055229f0
Juan Vieira <jmvieirar@gmail.com>
2022-03-26 11:28:00 +1100

    basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SuperheroesModule } from './modules/superheroes/superheroes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SuperheroesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
